459d097afbdc4913b023469579aff3688f7df2dc
vtu27602 <vtu27602@veltech.edu.in>
2026-08-07 10:09:36 +0530

    Add output example for session61.java

---
+++ Week 2/session61.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class BuildArrayPermutation {
+public static int[] buildArray(int[] nums) {
+int[] ans = new int[nums.length];
+for (int i = 0; i < nums.length; i++) {
+ans[i] = nums [nums [i]];
+}
+return ans;
+}
+public static void main(String[] args) {
+int[] nums = {0, 2, 1, 5, 3, 4};
+int[] result = buildArray(nums);
+System.out.println("Input: "+ Arrays.toString(nums));
+System.out.println("Output: "+ Arrays.toString(result));
+}
+}
+
+
+
+OUTPUT :
+Input: [0, 2, 1, 5, 3, 4]
+Output: [0, 1, 2, 4, 5, 3]
