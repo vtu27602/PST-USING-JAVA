d10d06cac4ea8c125b6e0b7ec034c8aca4b9d0a7
vtu27602 <vtu27602@veltech.edu.in>
2026-08-08 08:59:05 +0530

    Add altitude calculation program in session63.java
    Implement a program to calculate the highest altitude from a given gain array.

---
+++ Week 2/session63.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] gain = {-5, 1, 5, 0, -7};
+
+        int altitude = 0;
+        int highest = 0;
+
+        for (int i = 0; i < gain.length; i++) {
+
+            altitude = altitude + gain[i];
+
+            if (altitude > highest) {
+                highest = altitude;
+            }
+        }
+
+        System.out.println("Highest Altitude: " + highest);
+    }
+}
+
+
+OUTPUT :
+Highest Altitude: 1
