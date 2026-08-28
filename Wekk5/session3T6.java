1b18222e878a0717069482b2b0d10ba30ccf6d29
vtu27602 <vtu27602@veltech.edu.in>
2026-08-28 09:06:03 +0530

    Add alternatingCharacters function in Solution class
    Implement a solution to count deletions for alternating characters in a string.

---
+++ Wekk5/session3T6.java
@@ -0,0 +1,52 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static int alternatingCharacters(String s) {
+
+        int deletions = 0;
+
+        for (int i = 1; i < s.length(); i++) {
+
+            // If current character is same as previous
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                deletions++;
+            }
+        }
+
+        return deletions;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        int q = sc.nextInt();
+
+        while (q-- > 0) {
+
+            String s = sc.next();
+
+            System.out.println(alternatingCharacters(s));
+        }
+
+        sc.close();
+    }
+}
+
+
+INPUT :
+5
+AAAA
+BBBBB
+ABABABAB
+BABABA
+AAABBB
+  OUTPUT :
+  3
+4
+0
+0
+4
